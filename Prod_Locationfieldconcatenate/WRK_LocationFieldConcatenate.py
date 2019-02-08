e62a4467649a864d4ff68cf8f21592a7dae6f8c1
Anders Olson <23644837+andersolson@users.noreply.github.com>
2019-02-08 12:02:36 -0700

    added pks
    added script for parks trees

--- Prod_Locationfieldconcatenate/WRK_LocationFieldConcatenate.py
+++ Prod_Locationfieldconcatenate/WRK_LocationFieldConcatenate.py
@@ -7,7 +7,8 @@ from datetime import datetime
 
 # Setup the logfile name
 t = datetime.now()
-logFile = "C:/ScriptsForArcGIS/Prod/LocationFieldConcatenate/PROD_LocationFieldConcatenate"
+#logFile = "C:/ScriptsForArcGIS/Prod/LocationFieldConcatenate/PROD_LocationFieldConcatenate"
+logFile = "U:/AOLSON/Working/temp/PROD_LocationFieldConcatenate" #local test logfile
 logName = logFile + t.strftime("%y%m%d") + ".log"
 # Setup logging
 logger = logging.getLogger("PROD_LocationFieldConcatenate")
@@ -18,26 +19,39 @@ formatter = logging.Formatter('%(asctime)s - %(filename)s : line %(lineno)d - %(
 fh.setFormatter(formatter)
 logger.addHandler(fh)
 
-#Codeblock for ssGravityMain Concatenate: 
+#Codeblock for ssGravityMain concatenate: 
 # Row-by-row populate the LOCATION field with with the ASSETGOUP and TROUBLESPOT attributes.
-codeblock = """
+codeblock0 = """
 def loc_concat(ASSETGROUP,TROUBLESPOT):
         if TROUBLESPOT is None:
                 return ASSETGROUP
         return ASSETGROUP + " " + TROUBLESPOT"""
 
+#Codeblock for pksCWTrees concatenate: 
+# Row-by-row populate the LOCATION field with with the SUB_DISTRICT and PARK_NAME attributes.
+codeblock1 = """
+def loc_concat(SUB_DISTRICT,PARK_NAME):
+        if SUB_DISTRICT is None:
+                return PARK_NAME
+        return SUB_DISTRICT + "/" + PARK_NAME"""
+
 try:
         #Set the workspace
         logger.info("Set workspace environemnt")
-        arcpy.env.workspace = "C:\\ScriptsForArcGIS\\OPERATIONS - DEFAULT.sde"
+        #arcpy.env.workspace = "C:\\ScriptsForArcGIS\\OPERATIONS - DEFAULT.sde"
+        arcpy.env.workspace = r"U:\AOLSON\Working\temp\SDE_Copy.gdb" #local test workspace
 
-        #Set a variable for the workspace
-        logger.info("Set workspace environemnt as a variable")
+        #Define a variable for the workspace
+        logger.info("Define workspace environemnt as a variable")
         workspace = arcpy.env.workspace
 
         #Concatenate for "Location" field using "AssetGroup" and "TroubleSpot" 
         logger.info("Run concatenate for ssGravityMain LOCATION field")
-        arcpy.CalculateField_management("ssGravityMain", "LOCATION", "loc_concat(!ASSETGROUP!,!TROUBLESPOT!)", "PYTHON_9.3", codeblock)
+        arcpy.CalculateField_management("ssGravityMain", "LOCATION", "loc_concat(!ASSETGROUP!,!TROUBLESPOT!)", "PYTHON_9.3", codeblock0)
+        
+        #Concatenate for "Location" field using "Sub_District" and "Park_Name" 
+        logger.info("Run concatenate for pksCWTrees LOCATION field")
+        arcpy.CalculateField_management("pksCWTrees", "LOCATION", "loc_concat(!SUB_DISTRICT!,!PARK_NAME!)", "PYTHON_9.3", codeblock1)        
 
 except Exception as e:
         print e.args[0]
