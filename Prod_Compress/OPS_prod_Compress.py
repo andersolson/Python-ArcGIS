bdead1dffdd4127a48b7069367ed7cfc2e80c99c
Anders Olson <23644837+andersolson@users.noreply.github.com>
2019-03-18 09:44:05 -0600

    added messaging
    improved logger messages.

--- Prod_Compress/OPS_prod_Compress.py
+++ Prod_Compress/OPS_prod_Compress.py
@@ -24,37 +24,47 @@ try:
     arcpy.env.workspace = "C:\\ScriptsForArcGIS\\OPERATIONS - Arcadmin.sde"
 
     #Set a variable for the workspace
-    logger.info("Set a variable for the Workspace")
+    logger.info("Set a variable for the Workspace...")
     workspace = arcpy.env.workspace
+    logger.info("Workspace is: {0}".format(workspace))
+    
+    #Get a list of connected users.
+    logger.info("Get a list of connected users...")
+    userList = arcpy.ListUsers(workspace)
+    logger.info("Connected users are: {0}".format())
 
-    # block new connections to the database.
-    logger.info("block new connections to the database")
+    #Block new connections to the database.
+    logger.info("Block new connections to the database...")
     arcpy.AcceptConnections(workspace, False)
 
-    #disconnect all users from the database.
-    logger.info("disconnect all users from the database")
+    #Disconnect all users from the database.
+    logger.info("Disconnect all users from the database...")
     arcpy.DisconnectUser(workspace, "ALL")
 
-    # Get a list of versions to pass into the ReconcileVersions tool.
-    logger.info("Get a list of versions to pass into the ReconcileVersions tool")
+    #Get a list of versions to pass into the ReconcileVersions tool.
+    logger.info("Get a list of versions to pass into Reconcile Versions tool...")
     versionList = arcpy.ListVersions(workspace)
+    logger.info("Version list is: {0}".format(versionList))
 
-    # Execute the ReconcileVersions tool.
-    logger.info("Execute the ReconcileVersions tool")
+    #Execute the ReconcileVersions tool.
+    logger.info("Execute the Reconcile Versions tool...")
     arcpy.env.overwriteOutput = True
-    arcpy.ReconcileVersions_management(workspace, "ALL_VERSIONS", "dbo.DEFAULT", versionList, "LOCK_ACQUIRED", "ABORT_CONFLICTS", "BY_OBJECT", "FAVOR_TARGET_VERSION", "POST", "KEEP_VERSION", "C:\ScriptsForArcGIS\log\TESTreconcilelog.txt")
-    arcpy.ReconcileVersions_management(workspace, "BLOCKING_VERSIONS", "dbo.DEFAULT", versionList, "LOCK_ACQUIRED", "ABORT_CONFLICTS", "BY_OBJECT", "FAVOR_TARGET_VERSION", "POST", "KEEP_VERSION", "C:\ScriptsForArcGIS\log\TESTreconcilelog.txt")
+    arcpy.ReconcileVersions_management(workspace, "ALL_VERSIONS", "dbo.DEFAULT", versionList, "LOCK_ACQUIRED", "ABORT_CONFLICTS", "BY_OBJECT", "FAVOR_TARGET_VERSION", "POST", "KEEP_VERSION", "C:\ScriptsForArcGIS\log\ReconcileALL_log.txt")
+    arcpy.ReconcileVersions_management(workspace, "BLOCKING_VERSIONS", "dbo.DEFAULT", versionList, "LOCK_ACQUIRED", "ABORT_CONFLICTS", "BY_OBJECT", "FAVOR_TARGET_VERSION", "POST", "KEEP_VERSION", "C:\ScriptsForArcGIS\log\ReconcileBLOCK_log.txt")
+    logger.info("Reconcile complete.")
 
-    # Run the compress tool. 
-    logger.info("Run the compress tool")
+    #Run the compress tool. 
+    logger.info("Run the compress tool...")
     arcpy.Compress_management(workspace)
+    logger.info("Compress complete.")
 
-    # Allow new connections to the database.
-    logger.info("Allow new connections to the database")
+    #Allow new connections to the database.
+    logger.info("Allow new connections to the database...")
     arcpy.AcceptConnections(workspace, True)
+    logger.info("New connections allowed.")
 
 except Exception as e:
-    print e.args[0]
-    logger.exception(str(e))		
+    print(e.args[0])
+    logger.exception(str(e))
 
 logging.shutdown()
