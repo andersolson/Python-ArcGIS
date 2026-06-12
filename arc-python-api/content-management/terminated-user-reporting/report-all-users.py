1fb21cc5eb5caa8cb8f2b8455861d672855ad30b
aolson-c3 <aolson@c3gov.com>
2026-06-12 12:47:30 -0600

    ready to test user xlsx export
    ready to test user xlsx export

--- arc-python-api/content-management/terminated-user-reporting/report-all-users.py
+++ arc-python-api/content-management/terminated-user-reporting/report-all-users.py
@@ -15,6 +15,7 @@
 import arcgis
 from arcgis.gis import GIS
 import pandas as pd
+from datetime import datetime as dt
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 # ================================#
@@ -40,17 +41,65 @@ def outputError(msg):
 # ================================#
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 
+# None
+
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 # ================================#
 # Define variables and environments
 # ================================#
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 
+# Define variable for run start date & time as a string
+runStart = dt.now()
+
+# Define string format for the start time
+dtStr = runStart.strftime('%Y-%m-%d %H:%M:%S')
+mdyDT = runStart.strftime('%Y%m%d')
+
+# Define the working environment
+arcpy.env.workspace = str(workingGDB)
+
 # Connect to ArcGIS Online
 c3GIS = GIS(profile='aolson_prfl2')
+
+# Define working directory
+working_dir = r'C:\Users\is_olson\Documents\Projects\SSO_Update'
+arcpy.env.workspace = working_dir
+
+# Output file: excel sheet containing all agol users with date stamp
+all_users_xlsx = f'all_agol_users_{mdyDT}.xlsx'
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 # ================================#
 # Start calling functions
 # ================================#
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+
+# Output running message and start time
+outputMessage(f'Running: {sys.argv[0]}\nStart Time: {dtStr}')
+
+# Get a list of all org users
+all_users = c3GIS.users.search(query='*',max_users=666)
+outputMessage(f'Org has {len(all_users)} users')
+
+# 1. Convert user list to a pandas df
+# Start with a dictionary for the user data object from agol
+user_data = []
+for user in all_users:
+    user_data.append({
+        "username": user.username,
+        "fullName": user.fullName,
+        "email": user.email,
+        "role": user.role,
+        "type": user.userLicenseTypeId,
+        "disabled": user.disabled,
+        "lastLogin": user.lastLogin})
+
+# Convert dictionary to a dataframe
+df = pd.DataFrame(user_data)
+
+# Reformat the date from UNIX timestamp to readable text date
+df['lastLogin'] = pd.to_datetime(df['lastLogin'], unit='ms')
+
+# 2. Convert dataframe to excel sheet
+df.to_excel(all_users_xlsx, index=False)
