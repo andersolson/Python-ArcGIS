ba16a5c2f60fbb41e6803216333e80d6a4ed04e1
aolson-c3 <aolson@c3gov.com>
2026-06-12 12:32:22 -0600

    importing from notebook
    importing from notebook

---
+++ arc-python-api/content-management/terminated-user-reporting/report-all-users.py
@@ -0,0 +1,44 @@
+#-----------------------------------------------------------------------------
+# report-all-users.py
+#
+# Author: Anders Olson 2026
+#
+# Usage: Script is run as a stand-alone script
+#
+# Description: Script pulls a list of all users found in organization. Then,
+#              creates a dataframe for the user data and a dataframe for David's
+#              city employee list. Dataframes are joined and a report of active and
+#              terminated users is created.
+#
+#-------------------------------------------------------------------------------
+
+import arcgis
+from arcgis.gis import GIS
+import pandas as pd
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+# ================================#
+# Define Messenger
+# ================================#
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+
+def outputMessage(msg):
+    print(msg)
+    arcpy.AddMessage(msg)
+
+def outputWarning(msg):
+    print(msg)
+    arcpy.AddWarning(msg)
+
+def outputError(msg):
+    print(msg)
+    arcpy.AddError(msg)
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+# ================================#
+# Define functions
+# ================================#
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+
+# Connect to ArcGIS Online
+c3GIS = GIS(profile='aolson_prfl2')
