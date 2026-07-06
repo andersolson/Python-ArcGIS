866eb18f8fa722096679e015fbfbaee7067411e8
aolson-c3 <aolson@c3gov.com>
2026-07-06 16:17:36 -0600

    imported template and first steps
    imported template and first steps

--- arc-python-api/content-management/user-reporting/sso-ad-list.py
+++ arc-python-api/content-management/user-reporting/sso-ad-list.py
@@ -13,3 +13,48 @@
 #
 #-------------------------------------------------------------------------------
 
+from arcgis.gis import GIS
+import pandas as pd
+from datetime import datetime as dt
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+# ================================#
+# Define Messenger
+# ================================#
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+
+def outputMessage(msg):
+    print(msg)
+    arcpy.AddMessage(msg)
+
+def outputWarning(msg):
+    print(msg)
+    arcpy.AddWarning(msg)
+
+def outputError(msg):
+    print(msg)
+    arcpy.AddError(msg)
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+# ================================#
+# Define functions
+# ================================#
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+
+# None
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+# ================================#
+# Define variables and environments
+# ================================#
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+
+# Define variable for run start date & time as a string
+runStart = dt.now()
+
+# Define string format for the start time
+dtStr = runStart.strftime('%Y-%m-%d %H:%M:%S')
+mdyDT = runStart.strftime('%Y%m%d')
+
+# Connect to ArcGIS Online
+c3GIS = GIS(profile='aolson_prfl2')
